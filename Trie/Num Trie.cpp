3eaaa7c635ba978561d7799738bc08fce0342477
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2023-03-17 23:28:36 +0600

    Create Num Trie.cpp

---
+++ Trie/Num Trie.cpp
@@ -0,0 +1,43 @@
+struct node {
+    bool end;
+    node* next[11]; 
+    node() {
+        end = false;
+        for(int i = 0; i < 10; i++) {
+            next[i] = NULL;
+        }
+    }
+} *root;
+
+void insert(string str) {
+    node* curr = root;
+    int len = str.size();
+    for(int i = 0; i < len; i++) {
+        int id = str[i] - '0';
+        if(curr -> next[id] == NULL) {
+            curr -> next[id] = new node();
+        }
+        curr = curr -> next[id];
+    }
+    curr -> end = true;
+}
+
+bool search(string str) {
+    int len = str.size();
+    node* curr = root;
+    for(int i = 0; i < len; i++) {
+        int id = str[i] - '0';
+        if(curr -> next[id] == NULL) return false;
+        curr = curr -> next[id];
+    }
+    return curr -> end;
+}
+
+void del(node* curr) {
+    for (int i = 0; i < 10; i++) {
+        if (curr -> next[i]) {
+            del(curr -> next[i]);
+        }
+    }
+    delete(curr);
+}
