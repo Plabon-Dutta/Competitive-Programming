c90efb0a084086487c669f44f0dab1dd93b9fffa
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2023-04-18 20:02:26 +0600

    Update sieve.cpp

--- Number Theory/sieve.cpp
+++ Number Theory/sieve.cpp
@@ -1,19 +1,19 @@
-const int N=1e7+7;
-vector<int> prime;
+const int N=1e7 + 7;
+vector <int> prime;
 bool pchk[N];
 int cnt[N];
 
 
 void sieve() {
     prime.push_back(2);
-    pchk[0]=pchk[1]=true;
+    pchk[0] = pchk[1] = true;
     for(int i = 4; i < N; i += 2) {
         pchk[i] = true;
     }
     for(int i = 3; i * i < N; i += 2) {
         if(!pchk[i]) {
             for(int j = i + i; j < N; j += i) {
-                pchk[j]=  true;
+                pchk[j] =  true;
             }
         }
     }
