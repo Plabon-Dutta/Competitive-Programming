ba6231b21bdd5b094a02dd4a30729cce90324faa
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2023-06-05 19:56:26 +0600

    Create Divisible_pairs.cpp
    Problem:
    Given an array of length n and a number m.
    Find the number of pairs which follows the given condition.
    (a[i] + a[j]) % m == 0

---
+++ Others/Divisible_pairs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    int ans = 0;
+    map <int, int> mm;
+    
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        ans += (mm[m - (x % m)]);
+        mm[x % m]++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
